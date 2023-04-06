b7b31ea9df3143d7af3f2dede2b6426367c93bf8
Matej Kriz <matej@matejkriz.cz>
2023-04-06 23:24:28 +0200

    fix(layout): fix banner position for mobile

--- components/blocks/intro.tsx
+++ components/blocks/intro.tsx
@@ -9,42 +9,45 @@ import backgroundWide from "../../public/images/background-wide.webp";
 export const Intro = ({ data }) => {
   return (
     <Section color={data.color} className="relative w-full">
-      <Image
-        className={`relative max-h-[65vh] w-full lg:hidden`}
-        alt="Hravá dekorace"
-        src={background}
-        placeholder="blur"
-        priority
-      />
-      <Image
-        className={`relative max-h-[65vh] w-full hidden lg:block`}
-        alt="Hravá dekorace"
-        src={backgroundWide}
-        placeholder="blur"
-        priority
-      />
-      <Image
-        className={`absolute top-0 bottom-0 left-0 right-0 m-auto h-full max-h-[40vh] w-full max-w-[50vw] lg:max-w-[38vw]`}
-        alt="Habitat Zbraslav"
-        src="/images/habitat-zbraslav.svg"
-        width={374}
-        height={136}
-        priority
-      />
-      <div className="bg-orange-300 w-full opacity-90 absolute top-0">
-        <div className="max-w-screen-xl mx-auto px-4 py-3 text-white sm:text-center md:px-8">
-        <Link
-              href="/schuzka"
-              className="duration-150 hover:text-green-800"
-            >
-          <p className="text-md md:text-lg">
-            Zveme vás na&nbsp;setkání, které proběhne ve&nbsp;<span className="font-bold">čtvrtek 13.&nbsp;4. od&nbsp;17:30</span>!{' '}
-            <span
-              className="font-semibold underline inline-flex items-center gap-x-1"
-            >👉 Dozvědět se&nbsp;více</span>
-          </p>
+      <div className="bg-orange-300 w-full opacity-90 relative">
+        <div className="max-w-screen-xl mx-auto px-4 py-3 text-white text-center md:px-8">
+          <Link href="/schuzka" className="duration-150 hover:text-green-800">
+            <p className="text-md md:text-lg">
+              Zveme vás na&nbsp;setkání, které proběhne ve&nbsp;
+              <span className="font-bold">
+                čtvrtek 13.&nbsp;4. od&nbsp;17:30
+              </span>
+              ! 👉{" "}
+              <span className="font-semibold underline inline-flex items-center gap-x-1">
+                Dozvědět se&nbsp;více
+              </span>
+            </p>
           </Link>
         </div>
+      </div>
+      <div className="relative">
+        <Image
+          className={`relative max-h-[65vh] w-full lg:hidden`}
+          alt="Hravá dekorace"
+          src={background}
+          placeholder="blur"
+          priority
+        />
+        <Image
+          className={`relative max-h-[65vh] w-full hidden lg:block`}
+          alt="Hravá dekorace"
+          src={backgroundWide}
+          placeholder="blur"
+          priority
+        />
+        <Image
+          className={`absolute top-0 bottom-0 left-0 right-0 m-auto h-full max-h-[40vh] w-full max-w-[50vw] lg:max-w-[38vw]`}
+          alt="Habitat Zbraslav"
+          src="/images/habitat-zbraslav.svg"
+          width={374}
+          height={136}
+          priority
+        />
       </div>
     </Section>
   );
