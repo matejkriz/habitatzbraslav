2375bd11c7b70714c978adcb15f4b647bec9d479
Matej Kriz <matej@matejkriz.cz>
2023-04-19 23:31:08 +0200

    fix(form): responsive form

--- components/blocks/form.tsx
+++ components/blocks/form.tsx
@@ -6,12 +6,10 @@ import type { TinaTemplate } from "tinacms";
 export const Form = ({ data }) => {
   return (
     <Section color={data.color} className="relative w-full">
-        <Container className={`prose prose-lg overflow-y-auto`} size="medium" width="custom">
+        <Container className={`relative w-full overflow-hidden pt-[100%]`} size="medium" width="custom">
           <iframe
             src="https://docs.google.com/forms/d/e/1FAIpQLScKQrKo56qbBHuZn9eHT_oO0FHFWcymcGX8raAc6L3NpuhnAA/viewform?embedded=true"
-            width="100%"
-            height="2350"
-            className="border-0 margin-0"
+            className="absolute top-0 left-0 bottom-0 right-0 border-0 w-full h-full"
           >
             Načítání…
           </iframe>
